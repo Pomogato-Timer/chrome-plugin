4d51b1afea9e62894491b24010f726c650d073d8
qMaDMaNp <medifman@gmail.com>
2025-02-25 20:28:08 -0500

    Add concept test version

---
+++ service-worker.js
@@ -0,0 +1,59 @@
+let pomodoroWindowId = null;
+
+chrome.runtime.onMessage.addListener((message) => {
+    if (message.action === "startTimer") {
+        chrome.alarms.create("pomodoroTimer", { delayInMinutes: 0.1 });
+    } else if (message.action === "stopTimer") {
+        chrome.alarms.clear("pomodoroTimer");
+    }
+
+    console.log('pomodoroWindowId', pomodoroWindowId);
+    
+    if (pomodoroWindowId) {
+        chrome.windows.remove(pomodoroWindowId);
+        pomodoroWindowId = null; // Reset stored ID
+    }
+});
+
+chrome.alarms.onAlarm.addListener((alarm) => {
+    if (alarm.name === "pomodoroTimer") {
+        chrome.storage.local.remove("endTime"); // Clear stored time
+
+        // Try opening the extension popup (this requires user interaction first)
+        // chrome.action.openPopup().catch(() => {
+            // If popup cannot be opened, show a badge
+
+            //TODO: clear after few sec
+            chrome.action.setBadgeText({ text: "Done" });
+            chrome.action.setBadgeBackgroundColor({ color: "#FF4500" });
+
+            const aInterval = setInterval(() => {
+                chrome.action.setBadgeBackgroundColor({ color: "#FFF" });
+            }, 1000);
+
+            const bInterval = setInterval(() => {
+                chrome.action.setBadgeBackgroundColor({ color: "#FF4500" });
+            }, 2000);
+
+            //TODO: also clear timeouts after clicking on the plugin
+            setTimeout(() => {
+                clearInterval(aInterval);
+                clearInterval(bInterval);
+
+                chrome.action.setBadgeBackgroundColor({ color: "#FFF" });
+            }, 10000);
+
+            // Open a fallback window
+            chrome.windows.create({
+                url: "finished.html",
+                type: "popup",
+                width: 400,
+                height: 300
+            }, (window) => {
+                pomodoroWindowId = window.id; // Store the window ID
+            });
+        // });
+    }
+});
+
+
