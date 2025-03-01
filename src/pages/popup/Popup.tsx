06e64f175cf0088eb38bbd4df3df430fbd4098d9
qMaDMaNp <medifman@gmail.com>
2025-02-28 19:56:08 -0500

    Add time select buttons

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -4,12 +4,26 @@ import { useState, useRef } from 'react';
 import pomogatoLogo from '@assets/images/pomogato-logo.png'
 import startMessageImage from '@assets/images/start-message.png'
 
-import Clouds from '../../components/clouds/Clouds';
+import Clouds from './components/clouds/Clouds';
 
-const defaultTimer = '25:00';
+const times = [
+  {
+    id: 'default-1',
+    label: '25 min',
+    value: 25000
+  },
+  {
+    id: 'default-2',
+    label: '25 min',
+    value: 45000
+  }
+];
+
+const defaultTime = times[0];
 
 export default function Popup() {
-  const [timer, setTimer] = useState(defaultTimer);
+  const [timerTime, setTimerTime] = useState(defaultTime);
+
   const volumeInput = useRef(null);
 
   const timerDisplay = document.getElementById("timer");
@@ -45,28 +59,32 @@ export default function Popup() {
   function onStop() {
     chrome.storage.local.remove("endTime");
     chrome.runtime.sendMessage({ action: "stopTimer" });
-    setTimer(defaultTimer);
+    setTimerTime(defaultTime);
+  }
+
+  function onSetTimerTime() {
+    
   }
 
   function updateTimerUI() {
-    chrome.storage.local.get("endTime", (data) => {
-      if (data.endTime) {
-        const interval = setInterval(() => {
-          const remaining = Math.max(0, data.endTime - Date.now());
-          const minutes = Math.floor(remaining / 60000);
-          const seconds = Math.floor((remaining % 60000) / 1000);
+    // chrome.storage.local.get("endTime", (data) => {
+    //   if (data.endTime) {
+    //     const interval = setInterval(() => {
+    //       const remaining = Math.max(0, data.endTime - Date.now());
+    //       const minutes = Math.floor(remaining / 60000);
+    //       const seconds = Math.floor((remaining % 60000) / 1000);
 
-          let time = `${minutes}:${seconds.toString().padStart(2, "0")}`;
+    //       let time = `${minutes}:${seconds.toString().padStart(2, "0")}`;
 
-          if (remaining <= 0) {
-            clearInterval(interval);
-            time = '00:00';
-          }
+    //       if (remaining <= 0) {
+    //         clearInterval(interval);
+    //         time = '00:00';
+    //       }
 
-          setTimer(time);
-        }, 1000);
-      }
-    });
+    //       setTimerTime(time);
+    //     }, 1000);
+    //   }
+    // });
   };
 
   return (
@@ -80,6 +98,60 @@ export default function Popup() {
 
         <button style={{ marginTop: -24 }} className="boop-btn" onClick={onStart}>
           <img style={{ width: 88 }} src={pomogatoLogo} className="logo" alt="Start Timer Image" />
+        </button>
+      </div>
+
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: '4px',
+
+        marginTop: '0.5rem'
+      }}>
+        <button
+          onClick={onSetTimerTime}
+          style={{
+            borderRadius: '8px',
+            height: 32,
+            width: 64,
+            background: 'white',
+            border: '2px solid black',
+            fontWeight: 'bold',
+            // boxShadow: '0px 5px 3px -1px rgba(148,148,148,1)'
+          }}
+        >
+          25 min
+        </button>
+
+        <button
+          onClick={onSetTimerTime}
+          style={{
+            borderRadius: '8px',
+            height: 32,
+            width: 64,
+            background: 'white',
+            border: '2px solid black',
+            fontWeight: 'bold',
+            // boxShadow: '0px 5px 3px -1px rgba(148,148,148,1)'
+          }}
+        >
+          45 min
+        </button>
+
+        <button
+          onClick={onSetTimerTime}
+          style={{
+            borderRadius: '8px',
+            height: 32,
+            width: 64,
+            background: 'white',
+            border: '2px solid black',
+            fontWeight: 'bold',
+            // boxShadow: '0px 5px 3px -1px rgba(148,148,148,1)'
+          }}
+        >
+          ✏️ min
         </button>
       </div>
 
