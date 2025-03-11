7ba348f4c2cf88c0cc3b708ef91c351bc3a8134c
qMaDMaNp <medifman@gmail.com>
2025-03-11 16:51:52 -0400

    Add new alarm sound

--- src/pages/finished/Finished.tsx
+++ src/pages/finished/Finished.tsx
@@ -3,9 +3,13 @@ import React from 'react';
 import logo from '@assets/img/logo.svg';
 
 export default function Finished() {
-  let audio = new Audio(chrome.runtime.getURL("src/assets/alarm.mp3"));
-  audio.volume = 0.5; // Default to 50%
+  let audio = new Audio(chrome.runtime.getURL("src/assets/cat-meow-sound.mp3"));
+  audio.volume = 0.3; // Default to 30%
   audio.play();
+
+  setTimeout(() => {
+    audio.play();
+  }, 1500);
 
   return (
     <div className="absolute top-0 left-0 right-0 bottom-0 text-center h-full p-3 bg-gray-800">
